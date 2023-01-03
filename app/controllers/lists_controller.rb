86bf33ba3c9d3f5fdf5cdf0a318df119bc26dc8b
EC2 Default User <ec2-user@ip-172-31-11-156.ap-northeast-1.compute.internal>
2023-01-03 23:31:13 +0000

    [add]削除機能をつくろうまで

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,41 @@
 class ListsController < ApplicationController
   def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
   end
 
+
+  def create
+    #1.&2.データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    #3.データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    #4.トップ画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+
+
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  #.../lists/1や.../lists/3に該当する
+  get 'lists/:id' => 'lists#show', as:'list'
+  get'lists/:id/edit' => 'lists#edit', as:'edit_list'
+  patch 'list/:id' => 'lists#update', as:'update_list'
+
 end
